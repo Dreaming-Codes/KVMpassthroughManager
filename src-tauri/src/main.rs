86eb6842c783bee4351f1ce423d009bf5969245b
DreamingCodes <dev@dreaming.codes>
2021-11-05 18:55:57 +0100

    Implemented app minimum size
    Took 20 seconds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,14 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::{LogicalSize, Manager, Size};
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            app.get_window("main")
+                .unwrap()
+                .set_min_size(Option::Some(Size::Logical(LogicalSize { width: 600.0, height: 400.0 })))
+                .expect("ERROR IN MIN SIZE SET");
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
